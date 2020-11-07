9cf492815b30c397a6a7a0255a84208a65f28f66
Nick Anthony <nicholas_anthony@brown.edu>
2020-11-07 12:12:39 -0500

    Add create and read options to the Lesson5 app

--- Lesson5DummyToDoApp/app.py
+++ Lesson5DummyToDoApp/app.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nickyanthony@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    new_todo = request.form['newtodo']
+    NewTodo = Todo(description=new_todo)
+    db.session.add(NewTodo)
+    db.session.commit()
+    return render_template('index.html', data=Todo.query.all())
