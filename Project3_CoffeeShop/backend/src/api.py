7583593ea4d5cd303ae2b1beb68495efd8b7ca3b
Nick Anthony <nickanthony45@gmail.com>
2021-01-26 07:56:43 -0500

    Implement endpoint @app.route('/drinks-detail', methods=['GET'])

--- Project3_CoffeeShop/backend/src/api.py
+++ Project3_CoffeeShop/backend/src/api.py
@@ -40,14 +40,25 @@ def get_drinks(payload):
             })
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_details(payload):
+    drinks = Drink.query.all()
+    # If there are no drinks, throw a 404
+    if not drinks:
+        abort(404)
+    formatted_drinks = [drink.long() for drink in drinks]
+    return jsonify({
+                'success': True,
+                'drinks': formatted_drinks
+            })
 
 '''
 @TODO implement endpoint
